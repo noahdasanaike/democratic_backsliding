22b9f465392a6988749df4b1f496c39c38e4d118
Noah Dasanaike <noahdasanaike@college.harvard.edu>
2020-11-06 17:43:41 -0500

    finish first tabs

--- app.R
+++ app.R
@@ -9,6 +9,7 @@ library(ggthemes)
 library(modeest)
 library(countrycode)
 library(forecast)
+library(ggrepel)
 
 # Maps
 
@@ -37,6 +38,12 @@ pca_eui <- eui_subset %>%
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+    
+    tags$head(HTML("<title>Democracy Index</title>")),
+    tags$head(tags$style(type = "text/css", paste0(".selectize-dropdown {
+                                                     bottom: 100% !important;
+                                                     top:auto!important;
+                                                 }}"))),
 
     # Application title
     titlePanel(h1("How is Democracy Changing?", align = "center")),
@@ -48,24 +55,32 @@ ui <- fluidPage(
         tabPanel("Democracy Indices",
              plotOutput("distPlot"),
              fluidRow(column(3,
-                             selectInput(multiple = FALSE, "year1", "Year One:",
-                                         paste(unique(eui_subset$year))),
-                             selectInput(multiple = FALSE, "year2", "Year Two:",
-                                         paste(unique(eui_subset$year)))),
+                             selectInput(multiple = FALSE, selected = 2018,
+                                         "year1", "From:",
+                                         sort(as.numeric(paste(unique(eui_subset$year))),
+                                              decreasing = FALSE))),
+                      column(4,
+                             selectInput(multiple = FALSE, selected = 2019,
+                                         "year2", "To:",
+                                         sort(as.numeric(paste(unique(eui_subset$year))),
+                                              decreasing = TRUE))),
                       column(5,
-                             selectInput(multiple = TRUE, "region", "Regions:",
+                             selectInput(multiple = TRUE, selected = "Europe & Central Asia", 
+                                         "region", "Regions:",
                                          paste(unique(eui_subset$region)))))
         ),
-        tabPanel("Country Predictions",
+        tabPanel("Time Forecasting",
              plotOutput("predictPlot"),
              fluidRow(column(3,
-                             selectInput(multiple = TRUE, "country", "Countries:",
+                             selectInput(multiple = TRUE, 
+                                         selected = c("China", "Russian Federation", "United States"),
+                                         "country", "Countries:",
                                         paste(unique(eui_subset$country)))),
-                      column(4,
+                      column(4, selected = 4,
                              sliderInput("future", "n years in the future:",
                                          min = 0, max = 15,
                                          value = 2)),
-                      column(5,
+                      column(5, selected = 2,
                              sliderInput("poly", "y polynomial regression:",
                                          min = 1, max = 4,
                                          value = 2))
@@ -106,21 +121,28 @@ server <- function(input, output) {
             pivot_wider(names_from = year, values_from = standard) %>%
             group_by(country) %>% 
             summarise_all(funs(first(na.omit(.)))) %>%
+            mutate(change = as.factor(ifelse(get(input$year2) > get(input$year1), 
+                                             1, 0))) %>%
             ggplot(aes(x = get(input$year1), y = get(input$year2))) +
-            geom_text_repel(aes(label = country)) +
+            geom_text_repel(aes(label = country, color = change)) +
             labs(x = paste(unlist(input$year1)), y = paste(unlist(input$year2))) +
-            theme_bw()
+            theme_bw() +
+            geom_abline(slope = 1, intercept = 0, aes(alpha = 0.5)) +
+            scale_color_discrete(name = "Change", labels = c("Negative", "Positive"))
     })
     
     output$predictPlot <- renderPlot({
         eui_subset %>%
             filter(country %in% c(paste(unlist(input$country)))) %>%
             ggplot(aes(x = year, y = standard, group = country, color = country)) +
-            geom_line() +
+            geom_line(size = 1.1) +
             theme_bw() +
             xlim(1996, 2020 + input$future) +
             stat_smooth(method = "lm", formula = y~poly(x, input$poly), fullrange = TRUE, 
-                        se = FALSE)
+                        se = FALSE, lty = 2) +
+            labs(x = "Year", y = "Democratic Index") +
+            scale_color_discrete(name = "Countries") +
+            geom_vline(xintercept = 2019, size = 1.2)
     })
 }
 
